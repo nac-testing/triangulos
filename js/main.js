eef3645e23b032ad61defdc1392c97912414e6ca
Lucas Videla <videla.lucas@gmail.com>
2014-06-03 11:48:30 -0300

    Initial commit. Added dependencies so it's easier to download and test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * @author Lucas Videla <lucasvidela@clic.gob.ar>
+ * @author Emanuel Farfaro Ruiz <emanuelfarfaroruiz@clic.gob.ar>
+ * @version 0.1
+ * @note Application Main Javascript File
+ */
+
+// Application Namespace
+var TodosAprendiendo = TodosAprendiendo || {};
+
+// Behaviors
+
+// Modules
